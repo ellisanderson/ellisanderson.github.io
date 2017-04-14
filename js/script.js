ce305a639df3b9fc1cf5e4bc8cd6708748e9e7f4
ellisanderson <ellishenryanderson@gmail.com>
2017-04-14 16:00:36 -0400

    added more styling, jquery functionality, UI elements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.section-header').click(function(){
-    $(this).next().slideToggle();
+    $(this).next().slideToggle(1000);
     $(this).children('.section-header h2:nth-child(2), .section-header h2:nth-child(3)').toggle();
     // $(this).parent().siblings().slideToggle();
   });
@@ -8,8 +8,80 @@ $(document).ready(function(){
 
 
 
-$(document).ready(function(){
 
+$(document).ready(function(){
   window.sr = ScrollReveal();
   sr.reveal('header, .section-header, .box', { duration: 1500 });
+});
+
+
+
+$(document).ready(function(){
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+            $('#back-to-top').fadeIn("slow");
+        } else {
+            $('#back-to-top').fadeOut("slow");
+        }
+  });
+
+  $('#back-to-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+});
+
+
+
+// $(document).ready(function(){
+//   $('#back-to-top').click(function(){
+//     $("html, body").animate({
+//             scrollTop: 0
+//         }, 600);
+//         return false;
+//     });
+//   });
+// });
+
+
+
+
+$(document).ready(function(){
+  lightbox.option({
+    'disableScrolling' : true
+  });
+});
+
+
+
+
+$(document).ready(function(){
+
+  $('#co').click(function() {
+    $('html, body').animate({
+            scrollTop: $('#coffee').offset().top
+          }, 700);
+  });
+
+  $('#gr').click(function() {
+    $('html, body').animate({
+            scrollTop: $('#goodreads').offset().top
+          }, 700);
+  });
+
+  $('#np').click(function() {
+    $('html, body').animate({
+            scrollTop: $('#nonprofit').offset().top
+          }, 700);
+  });
+
+  $('#fl').click(function() {
+    $('html, body').animate({
+            scrollTop: $('#freelance').offset().top
+          }, 700);
+  });
+
 });
